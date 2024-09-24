705e150fa4c3c46472a06fe409972d4a769bb3ee
divyaja <divyajapaidipamula@gmail.com>
2024-09-24 15:04:17 +0530

    writing some code in java script

---
+++ script.js
@@ -0,0 +1,50 @@
+//DataTypes
+let name = "DIVYAJA";
+console.log(typeof(name));
+
+let digit = 50;
+console.log(typeof(digit));
+
+let a = true;
+console.log(typeof(a));
+
+let b = undefined;
+console.log(typeof(b));
+
+let mynull = null;
+console.log(typeof(mynull));
+
+//Mathematical Operators
+let x = 10;
+let y = 10;
+console.log("10+10 = ",x+y);
+
+let i = 50;
+let j = 20;
+console.log("50-20 = ",i-j);
+
+let num_1 = 5;
+let num_2 = 10;
+console.log("5*10 = ",num_1*num_2);
+
+let num1 = 4;
+let num2 = 2;
+console.log("4/2 = ",num1/num2);
+
+let c = 10;
+let d = 5;
+console.log("10%5 = ",c%d);
+
+//Comparator Operators
+let number_1 = 10;
+let number_2 = "10";
+let number_3 = 30;
+console.log("number_1 == number_2", number_1==number_2);
+console.log("number_1 === number_2", number_1===number_2);
+console.log("number_1 != number_3", number_1 != number_3);
+console.log("number_1 !== number_3", number_1 !== number_3);
+console.log("number_1 > number_3", number_1 > number_3);
+console.log("number_1 < number_3", number_1 < number_3);
+console.log("number_1 >= number_2", number_1 >= number_2);
+console.log("number_1 <= number_3", number_1 <= number_3);
+
